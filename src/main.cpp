39ab76e6f499e785c80dccfeca9154cebb7146a7
Thomas Pelletier <pelletier.thomas@gmail.com>
2017-05-26 21:21:54 -0700

    Mouse movements

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,18 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     }
 }
 
+static bool first_mouse_pos = true;
+static GLfloat mouse_last_x = 0;
+static GLfloat mouse_last_y = 0;
+static GLfloat mouse_x = 0;
+static GLfloat mouse_y = 0;
+static const GLfloat mouse_sensitivity = 0.05f;
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+    mouse_x = (GLfloat) xpos;
+    mouse_y = (GLfloat) ypos;
+}
+
 int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -53,6 +65,10 @@ int main() {
     glEnable(GL_DEPTH_TEST);
 
     glfwSetKeyCallback(window, key_callback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    mouse_last_x = width / 2;
+    mouse_last_y = height / 2;
 
     std::cout << "Loading shaders" << std::endl;
     shader::Shader simple_shader("simple.vert", "simple.frag");
@@ -66,16 +82,55 @@ int main() {
     Renderer renderer;
 
     GLfloat last_time = 0.0f;
+
+    GLfloat camera_pitch = 0.0f;
+    GLfloat camera_yaw = -90.0f;
+
     while (!glfwWindowShouldClose(window)) {
         GLfloat current_time = (GLfloat) glfwGetTime();
         GLfloat delta_time = current_time - last_time;
 
         glfwPollEvents();
 
-        // Process keyboard events
+        // Process camera-related events
+
+        if (first_mouse_pos) {
+            first_mouse_pos = false;
+            mouse_last_y = mouse_y;
+            mouse_last_x = mouse_x;
+        }
+
+        GLfloat mouse_x_offset = (GLfloat) (mouse_x - mouse_last_x) * mouse_sensitivity;
+        GLfloat mouse_y_offset = (GLfloat) (mouse_last_y - mouse_y) * mouse_sensitivity; // reverse to enable Y inversion
+        mouse_last_x = mouse_x;
+        mouse_last_y = mouse_y;
+
+        camera_pitch += mouse_y_offset;
+        if (camera_pitch > 89.0f) {
+            camera_pitch = 89.0f;
+        } else if (camera_pitch < -89.0f) {
+            camera_pitch = -89.0f;
+        }
+
+        camera_yaw += mouse_x_offset;
+
+        std::cout << "mouse_x_offset= " << mouse_x_offset << " mouse_y_offset=" << mouse_y_offset << " pitch = " << camera_pitch << " yaw = " << camera_yaw << std::endl;
+
         static const GLfloat camera_speed = delta_time * 0.05f;
         static glm::vec3 camera_pos(0.0f, 0.0f, 3.0f);
-        static glm::vec3 camera_direction(0.0f, 0.0f, -1.0f);
+
+
+        GLfloat cos_pitch = glm::cos(glm::radians(camera_pitch));
+        GLfloat sin_pitch = glm::sin(glm::radians(camera_pitch));
+        GLfloat cos_yaw = glm::cos(glm::radians(camera_yaw));
+        GLfloat sin_yaw = glm::sin(glm::radians(camera_yaw));
+
+        std::cout << "PITCH=" << camera_pitch << " COS()=" << cos_pitch << " SIN()=" << sin_pitch << std::endl;
+        std::cout << "YAW=" << camera_yaw << " COS()=" << cos_yaw << " SIN()=" << sin_yaw << std::endl;
+
+        glm::vec3 camera_direction = glm::normalize(glm::vec3(cos_pitch * cos_yaw,
+                                                              sin_pitch,
+                                                              cos_pitch * sin_yaw));
         static glm::vec3 camera_up(0.0f, 1.0f, 0.0f);
 
         if (keys[GLFW_KEY_W]) {
@@ -91,12 +146,13 @@ int main() {
             camera_pos += camera_speed * glm::normalize(glm::cross(camera_direction, camera_up));
         }
 
-//        const GLfloat time = (const GLfloat) glfwGetTime();
-
         // Compute camera
+        std::cout << "direction = " << glm::to_string(camera_direction) << std::endl;
         glm::vec3 camera_target(camera_pos + camera_direction);
         glm::mat4 view = projection * glm::lookAt(camera_pos, camera_target, camera_up);
 
+
+        // Render scene!
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
